c1cb415625ab3d5c23561fa1b4cb7def81ed35a0
Anmol Agrawal <anmol.ag53@gmail.com>
2021-08-23 22:18:45 +0530

    Added Basic Layout

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-alert('Hi');
+window.addEventListener("DOMContentLoaded", (event) => {
+	const body = document.querySelector("body");
+	const { backgroundColor, backgroundImage } = randomGradientGenerator();
+	body.style.backgroundColor = backgroundColor;
+	body.style.backgroundImage = backgroundImage;
+});
+
+function randomColorGenerator() {
+	let x = Math.random();
+	let randomColor = Math.floor(x * 16777215).toString(16);
+	return `#${randomColor}`;
+}
+
+function randomGradientGenerator() {
+	let randomColor1 = randomColorGenerator();
+	let randomColor2 = randomColorGenerator();
+	return {
+		backgroundColor: `#${randomColor1}`,
+		backgroundImage: `linear-gradient(90deg, ${randomColor1} 0%, ${randomColor2} 100%)`,
+	};
+}
