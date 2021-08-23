9fe401b9447d03176e9e56c37633531ebf7e5a20
Anmol Agrawal <anmol.ag53@gmail.com>
2021-08-23 23:12:03 +0530

    Added Functionality

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ window.addEventListener("DOMContentLoaded", (event) => {
 	const { backgroundColor, backgroundImage } = randomGradientGenerator();
 	body.style.backgroundColor = backgroundColor;
 	body.style.backgroundImage = backgroundImage;
+    customizedGradient();
 });
 
 function randomColorGenerator() {
@@ -11,11 +12,27 @@ function randomColorGenerator() {
 	return `#${randomColor}`;
 }
 
-function randomGradientGenerator() {
-	let randomColor1 = randomColorGenerator();
-	let randomColor2 = randomColorGenerator();
+function randomGradientGenerator({angle='90deg', randomColor1 = randomColorGenerator(), randomColor2 = randomColorGenerator()} = {}) {
 	return {
-		backgroundColor: `#${randomColor1}`,
-		backgroundImage: `linear-gradient(90deg, ${randomColor1} 0%, ${randomColor2} 100%)`,
+		backgroundColor: `${randomColor1}`,
+		backgroundImage: `linear-gradient(${angle}, ${randomColor1} 0%, ${randomColor2} 100%)`,
 	};
+}
+
+const customizedGradient = (input) => {
+    document.querySelector('.copied').classList.remove('copied-active');
+	const { backgroundColor, backgroundImage } = randomGradientGenerator();
+	const ele = document.querySelector("#gradient_container");
+	ele.style.backgroundColor = backgroundColor;
+	ele.style.backgroundImage = backgroundImage;
+    console.log('Hi');
+    const output_display = document.querySelector('#gradient_output');
+    console.log('1', output_display);
+    output_display.innerHTML = `background: ${backgroundColor};<br/>background-image: ${backgroundImage};`;
+};
+
+const copyCSS = () => {
+    const copyText = document.querySelector("#gradient_output").innerHTML.replace('<br>', '\n').replace('<br/>', '\n');
+    navigator.clipboard.writeText(copyText);
+    document.querySelector('.copied').classList.add('copied-active');
 }
